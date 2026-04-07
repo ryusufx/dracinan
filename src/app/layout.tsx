929707a07cd7d1106f5d9ea0bd4051aa12311d7a
ryusufx <ryusuf.jr@gmail.com>
2026-04-08 00:49:08 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,8 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Suspense } from "react";
 
 export const metadata: Metadata = {
-  title: "SekaiDrama - Streaming Drama Pendek",
-  description: "Nonton drama pendek gratis dan tanpa iklan di SekaiDrama.",
+  title: "Dracinan - Streaming Drama Pendek",
+  description: "Nonton drama pendek gratis dan tanpa iklan di Dracinan.",
 };
 
 export default function RootLayout({
